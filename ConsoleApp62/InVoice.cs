ebcc91b1f35c701b1dcc9362eaf84e2ee088edcb
naimar1996 <naimar@code.edu.az>
2023-02-04 17:14:02 +0400

    Encapsulation

---
+++ ConsoleApp62/InVoice.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp62
+{
+    public class Invoice
+    {
+        private int account;
+        private string customer;
+        private string provider;
+        public string Article { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        private string article;
+        private int quantity;
+        private decimal price;
+
+        public Invoice(int account, string customer, string provider)
+        {
+            this.account = account;
+            this.customer = customer;
+            this.provider = provider;
+        }
+        public decimal CostCalculation(bool needEdv)
+        {
+            if (needEdv == true)
+            {
+                return (Price * Quantity) * 1.18m;
+            }
+            else
+            {
+                return Price * Quantity;
+            }
+        }
+    }
+
+}
---
+++ ConsoleApp62/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp62
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice seafood = new Invoice(123456, "Person", "Bazaa Store");
+            seafood.Article = "Salmon";
+            seafood.Quantity = 2;
+            seafood.Price = 25;
+            Console.WriteLine(seafood.CostCalculation(true));
+        }
+    }
+}
